a3846839f895a6491ff28afa1913833b8c9a7125
Salih Çiftçi <salih@ciftci.co>
2018-06-29 17:22:36 +0300

    added live/stopped/all func for /containers

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -16,6 +16,49 @@ function Search() {
     }
 }
 
+function Live() {
+    var filter, table, tr, td, i;
+    filter = "fa-check";
+    table = document.getElementById("table");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[6];
+        if (td) {
+            if (td.innerHTML.indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+
+function Stopped() {
+    var filter, table, tr, td, i;
+    filter = "fa-times";
+    table = document.getElementById("table");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[6];
+        if (td) {
+            if (td.innerHTML.indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+
+function All() {
+    var  table, tr, i;
+    table = document.getElementById("table");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        tr[i].style.display = "";
+    }
+}
+
 function Copy() {
     var copyText = document.getElementById("key");
     copyText.select();
