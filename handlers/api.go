c320bf0b72db4f58eab58ed9635993688898ad29
Salih Çiftçi <salih@ciftci.co>
2018-07-08 21:18:23 +0300

    licensing

--- handlers/api.go
+++ handlers/api.go
@@ -1,3 +1,7 @@
+// Copyright 2018 The Liman Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
 package handlers
 
 import (
@@ -223,35 +227,4 @@ func apiLogs(w http.ResponseWriter, r *http.Request) {
 		})
 
 	log.Println(r.Method, r.URL.Path)
-}
-
-//Handler asd
-func Handler() http.Handler {
-	r := http.NewServeMux()
-
-	r.HandleFunc("/", indexHandler)
-	r.HandleFunc("/containers", containersHandler)
-	r.HandleFunc("/stats", statsHandler)
-	r.HandleFunc("/images", imagesHandler)
-	r.HandleFunc("/volumes", volumesHandler)
-	r.HandleFunc("/networks", networksHandler)
-	r.HandleFunc("/logs", logsHandler)
-	r.HandleFunc("/settings", settingsHandler)
-
-	r.HandleFunc("/login", loginHandler)
-	r.HandleFunc("/logout", logoutHandler)
-	r.HandleFunc("/install", installHandler)
-
-	r.HandleFunc("/notifications", notificationHandler)
-
-	r.HandleFunc("/api/containers", apiContainer)
-	r.HandleFunc("/api/images", apiImages)
-	r.HandleFunc("/api/volumes", apiVolumes)
-	r.HandleFunc("/api/networks", apiNetworks)
-	r.HandleFunc("/api/stats", apiStats)
-	r.HandleFunc("/api/logs", apiLogs)
-	r.HandleFunc("/api/status", apiStatus)
-
-	r.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
-	return r
 }
--- main.go
+++ main.go
@@ -2,19 +2,17 @@ package main
 
 import (
 	"log"
-	"net/http"
 
 	"github.com/salihciftci/liman/cmd"
 	"github.com/salihciftci/liman/handlers"
 )
 
 func main() {
-	//Checking Notifications every 5 second
 	cmd.CheckNotifications()
 
 	log.Println("Listening http://0.0.0.0:8080")
-	err := http.ListenAndServe(":8080", handlers.Handler())
+	err := handlers.HTTPServer().ListenAndServe()
 	if err != nil {
-		panic(err)
+		log.Println(err)
 	}
 }
