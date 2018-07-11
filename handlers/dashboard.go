67c05cc330e8513529d6d7d1f61c7f24d29213bf
Salih Çiftçi <salih@ciftci.co>
2018-07-11 19:35:49 +0300

    removed post method

--- handlers/dashboard.go
+++ handlers/dashboard.go
@@ -7,33 +7,11 @@ package handlers
 import (
 	"log"
 	"net/http"
-	"time"
 
 	"github.com/salihciftci/liman/cmd"
-	"github.com/salihciftci/liman/util"
-)
-
-var (
-	username = "root"
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
-		inputPass := r.FormValue("inputPassword")
-		inputUser := r.FormValue("inputUser")
-		match := util.CheckPass(inputPass, userPassword)
-		if inputUser == username && match {
-			cookie := &http.Cookie{
-				Name:    "session",
-				Value:   cookieValue,
-				Path:    "/",
-				Expires: time.Now().AddDate(2, 0, 0),
-				MaxAge:  0,
-			}
-			http.SetCookie(w, cookie)
-		}
-	}
-
 	err := parseSessionCookie(w, r)
 	if err != nil {
 		return
