255d576d5d44bc4665405bdcd26444d8868d9a4f
Salih Çiftçi <salih@ciftci.co>
2019-07-23 23:01:38 +0300

    added hbs

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 const express = require("express");
 const path = require("path");
 const logger = require("morgan");
+const hbs = require("express-hbs");
+const cookieParser = require("cookie-parser");
 
-const auth = require("./routes/middlewares/authenticate");
+const header = require("./routes/middlewares/header");
+const cookie = require("./routes/middlewares/cookie");
 
 const app = express();
 
-app.use(logger("dev"));
+app.use(logger("dev")); //todo change in production
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-// Serving React files
-app.use(express.static(path.join(__dirname, "build"))); //todo
+// Handlebars configs
+app.set("view engine", "tmpl");
+app.set("views", __dirname + "/templates");
+app.engine("tmpl", hbs.express4({
+    partialsDir: __dirname + "/templates/partials/",
+    extname: ".tmpl"
+}));
+
+// Serving Static files
+app.use(express.static(path.join(__dirname, "public")));
 
 // Using middleware
-app.use(auth);
+app.use(header, cookie);
 
 // API Routes
 app.use("/api/users", require("./routes/api/users"));
@@ -23,5 +35,8 @@ app.use("/api/images/", require("./routes/api/images"));
 app.use("/api/volumes/", require("./routes/api/volumes"));
 app.use("/api/networks/", require("./routes/api/networks"));
 app.use("/api/", require("./routes/api/index"));
+
+app.use("/", require("./routes/web/index"));
+app.use("/login", require("./routes/web/login"));
 
 module.exports = app;
