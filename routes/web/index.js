268a1ecaa6f2e693b4f31b29e8f966f2f017c7b3
Salih Çiftçi <salih@ciftci.co>
2019-07-27 01:13:25 +0300

    rendering

--- routes/web/index.js
+++ routes/web/index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const Docker = require("../../lib/docker");
 
 router.get("/", async (req, res) => {
     try {
         res.render("index", {
-            title: "tesrt"
+            title: "Home",
+            containersCount: await Docker.Container.count(),
+            imagesCount: await Docker.Image.count(),
+            volumesCount: await Docker.Volume.count(),
+            networksCount: await Docker.Network.count(),
+            docker: await Docker.Docker.info()
         });
     } catch (e) {
         console.log(e);
