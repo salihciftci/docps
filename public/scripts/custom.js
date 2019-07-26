feaf7ac4a939558c093cf6789d0ec8d00a8ede6e
Salih Çiftçi <salih@ciftci.co>
2019-07-27 01:11:58 +0300

    init

---
+++ public/scripts/custom.js
@@ -0,0 +1,3 @@
+let pageName = document.getElementById("pageName").innerHTML.toLowerCase();
+let sidebarElement = document.getElementById(pageName);
+sidebarElement.style.backgroundColor = "#e9ecef";
