388bdaa2449ea26d5e5e866ee5cc0dc44722fc95
Salih Çiftçi <salih@ciftci.co>
2019-07-28 03:26:13 +0300

    inspecting containers

--- routes/web/containers.js
+++ routes/web/containers.js
@@ -5,6 +5,14 @@ const { Container } = require("../../lib/docker");
 router.get("/", async (req, res) => {
     try {
         let containers = await Container.ls();
+
+        for (let [i, container] of containers.entries()) {
+            containers[i] = await Container.inspect(container.id);
+            containers[i].name = container.name.charAt(0).toUpperCase() + container.name.slice(1);
+        }
+
+        console.log(containers[0]);
+
         res.render("containers", {
             title: "Containers",
             containers: containers
