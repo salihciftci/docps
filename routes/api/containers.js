bd31f606595335dbf367a3120df3ebb14af7fef0
Salih Çiftçi <salih@ciftci.co>
2019-07-28 03:26:27 +0300

    add inspect route

--- routes/api/containers.js
+++ routes/api/containers.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const Docker = require("../../lib/docker");
+const { Container } = require("../../lib/docker");
 
 router.get("/", async (req, res) => {
     try {
-        let containers = await Docker.Container.ls();
+        let containers = await Container.ls();
+        containers.forEach((e, i) => {
+            delete containers[i].IP;
+            delete containers[i].restartPolicy;
+            delete containers[i].volumes;
+        });
         res.json(containers);
     } catch (e) {
         console.log(e);
@@ -12,8 +17,18 @@ router.get("/", async (req, res) => {
     }
 });
 
-router.get("/:id", function (req, res) {
-
+router.get("/:id", async (req, res) => {
+    try {
+        let id = req.params.id;
+        let containers = await Container.inspect(id);
+        res.json(containers);
+    } catch (e) {
+        if (e.code === 404) {
+            res.sendStatus(404);
+            return;
+        }
+        res.sendStatus(500);
+    }
 });
 
 router.get("/:name/logs", async (req, res) => {
@@ -21,7 +36,7 @@ router.get("/:name/logs", async (req, res) => {
     let containerName = req.params.name;
     let lineCount = req.body.lines || 10;
     try {
-        let logs = await Docker.Container.logs(containerName, lineCount);
+        let logs = await Container.logs(containerName, lineCount);
         res.json(logs);
     } catch (e) {
         console.log(e);
