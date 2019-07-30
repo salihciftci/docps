64f6f4febec045325afb6e14f3135fcc5391e82a
Salih Çiftçi <salih@ciftci.co>
2019-07-30 21:33:59 +0300

    Add search input and shortcut buttons for listing

--- public/scripts/custom.js
+++ public/scripts/custom.js
@@ -1,3 +1,70 @@
 let pageName = document.getElementById("pageName").innerHTML.toLowerCase();
 let sidebarElement = document.getElementById(pageName);
 sidebarElement.style.backgroundColor = "#e9ecef";
+
+// eslint-disable-next-line
+function containerSearch() {
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("containerInput");
+    filter = input.value.toUpperCase();
+
+    table = document.getElementById("containerTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+
+// eslint-disable-next-line
+function showLiveContainers() {
+    console.log("click");
+    var filter, table, tr, td, i;
+    filter = "fa-check";
+    table = document.getElementById("containerTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[4];
+        if (td) {
+            if (td.innerHTML.indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+
+// eslint-disable-next-line
+function showStoppedContainers() {
+    var filter, table, tr, td, i;
+    filter = "fa-times";
+    table = document.getElementById("containerTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[4];
+        if (td) {
+            if (td.innerHTML.indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+
+// eslint-disable-next-line
+function showAllContainers() {
+    var table, tr, i;
+    table = document.getElementById("containerTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        tr[i].style.display = "";
+    }
+}
